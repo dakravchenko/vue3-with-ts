d6012c502d02cc07a4c696db6124725bcdc89b3a
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-03 17:04:06 +0200

    ts in components and props

--- hyrule/src/App.vue
+++ hyrule/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="app">
-    <p></p>
+    <JobList :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 import Job from './types/Job'
-
+import JobList from './components/JobList.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {JobList},
   setup(){
     // const state = reactive({
     //   name: 'Link',
